70a6a6f94aa764e94f4155906c6e66fee9d7aa76
Jacob <jacobmatthewmurray@gmail.com>
2017-04-19 19:25:25 +0200

    tables mod

--- orgmgm/tables.py
+++
@@ -1,8 +0,0 @@
-import django_tables2 as tables
-from .models import Organisation
-
-class OrgaTable(tables.Table):
-    edit_column = tables.TemplateColumn('<a href="">Edit</a>', orderable=False)
-    class Meta:
-        model = Organisation
-        attrs = {'class' : 'paleblue'}
